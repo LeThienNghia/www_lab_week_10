28b4e3c5f222c5fd85c50a955405a28750014614
LeThienNghia <lethiennghia1705@gmail.com>
2023-11-26 08:43:26 +0700

    create app

---
+++ src/main/java/vn/edu/iuh/fit/www_lab_week_10/WwwLabWeek10Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_lab_week_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwLabWeek10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwLabWeek10Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/www_lab_week_10/WwwLabWeek10ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_lab_week_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwLabWeek10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
